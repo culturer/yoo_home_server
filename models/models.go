32058b8473e1b59cfa7cd015afb7d8e0f435c7e7
cultuler <78901214@qq.com>
2018-03-19 16:08:17 +0800

    修改model配置

--- models/models.go
+++ models/models.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -9,8 +10,12 @@ func RegiesterDB() {
 
 	//注册驱动
 	orm.RegisterDriver("mysql", orm.DRMySQL)
+	//数据库链接
 	//注册默认数据库
-	orm.RegisterDataBase("default", "mysql", "root:78901214@tcp(127.0.0.1:3306)/yooplus_test1?charset=utf8")
+	var db_url string = beego.AppConfig.String("username_DB") + ":" + beego.AppConfig.String("password_DB") + "@tcp(" + beego.AppConfig.String("host_DB") + ")/" + beego.AppConfig.String("name_DB") + "?charset=" + beego.AppConfig.String("charset")
+	beego.Info(db_url)
+	orm.RegisterDataBase("default", "mysql", db_url)
+	// orm.RegisterDataBase("default", "mysql", "root:78901214@tcp(127.0.0.1:3306)/yoo_home?charset=utf8")
 	//注册model
 	orm.RegisterModel(new(TUser), new(TUserArrangement))
 	orm.RegisterModel(new(TFamily), new(TFamilyActivity))
